407f1df4432cfad7c6452328ece44a3d9acdc115
Denis <krollik1000000@gmail.com>
2020-06-18 12:20:13 +0300

    a

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "../src/js/slaider.js";
 
 let count = 0;
 const list = document.querySelectorAll(".list");
-
+const send = document.querySelector(".send");
 function check(arr, count) {
   for (let i = 0; i <= arr.length; i++) {
     if (+i !== count && arr[i]) {
@@ -22,3 +22,27 @@ for (let elem = 0; elem <= list.length; elem++) {
     });
   }
 }
+
+(function(){
+  emailjs.init("user_N5JR8YYD4ZUE9uPaypxsL");
+})();
+
+var templateParams = {
+  name: "denis",
+  phone: document.querySelector(".phone").value,
+  email: "krollik1000000@gmail.com"
+};
+
+function message () {
+   emailjs.send('krollik1000000_gmail_com', 'template_b5PgxmW1', templateParams)
+  .then(function(response) {
+     console.log('SUCCESS!', response.status, response.text);
+  }, function(error) {
+     console.log('FAILED...', error);
+  });
+}
+
+send.addEventListener("click", () => {
+  message()
+  console.log(document.querySelector(".name").value)
+})
